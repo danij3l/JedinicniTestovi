d69ab61f67e3ec9c52004df07403478bcd8022be
Danijel Pavić <danijelpavic21@gmail.com>
2017-03-01 21:07:51 +0100

    Napravljeni inicijalni testovi.

---
+++ KompleksniBroj/KompleksniBroj.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom
+{
+    public struct KompleksniBroj
+    {
+        public KompleksniBroj(double realni, double imaginarni)
+        {
+            realniDio = realni;
+            imagrinarniDio = imaginarni;
+        }
+
+        public double realniDio;
+        public double imagrinarniDio;
+
+        public static KompleksniBroj operator+(KompleksniBroj br1, KompleksniBroj br2)
+        {
+            return new KompleksniBroj(br1.realniDio + br2.realniDio, br1.imagrinarniDio + br2.imagrinarniDio);
+        }
+    }
+}
---
+++ Testovi/TestKompleksnogBroja.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom;
+
+namespace Testovi
+{
+    [TestClass]
+    public class TestKompleksnogBroja
+    {
+        [TestMethod]
+        public void PozivKontruktoraRealnogBrojaKaoRealniDioPostavljaPrviArgument()
+        {
+            KompleksniBroj kb = new KompleksniBroj(4, 7);
+            Assert.AreEqual(4, kb.realniDio);
+        }
+        [TestMethod]
+        public void PozivKontruktoraRealnogBrojaKaoImaginarniDioPostavljaDrugiArgument()
+        {
+            KompleksniBroj kb = new KompleksniBroj(4, 7);
+            Assert.AreEqual(7, kb.imagrinarniDio);
+        }
+
+        [TestMethod]
+        public void ZbrojDvaKompleksnaBrojaDajeKompleksniBrojSRealnimDIjelomJednakZbrojuRelnihDIjelova()
+        {
+            KompleksniBroj kb1 = new KompleksniBroj(4, 7);
+            KompleksniBroj kb2 = new KompleksniBroj(7, 9);
+            Assert.AreEqual(11, (kb1+kb2).realniDio);
+        }
+
+        [TestMethod]
+        public void ZbrojDvaKompleksnaBrojaDajeKompleksniBrojSImaginarnimDijelomJednaimZbrojuImaginarnihDijelova()
+        {
+            KompleksniBroj kb1 = new KompleksniBroj(4, 7);
+            KompleksniBroj kb2 = new KompleksniBroj(7, 9);
+            Assert.AreEqual(16, (kb1 + kb2).imagrinarniDio);
+        }
+    }
+}
